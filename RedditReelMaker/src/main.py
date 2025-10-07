b242ba5e0c3fe933f9040f842917be915c0b3eb9
rd <hopartean.radu@spectrum.ro>
2025-10-07 19:39:14 +0300

    api changes

--- RedditReelMaker/src/main.py
+++ RedditReelMaker/src/main.py
@@ -1,7 +1,15 @@
 
-from rrm_api.rrm import rrm_api
-from rrm_api.sound import sound
+from rrm_api.rrm import Api
+from rrm_api.sound import Sound
+from rrm_api.video import  Video
 
-api = rrm_api(100,200,"AskReddit")
-
+api = Api(100,200,"AskReddit")
 selected = api.get_post(20,40)
+api.sanitize_comment(selected)
+
+audio_path = Sound(selected).do()
+
+video = Video(audio_path)
+output_file = video.do()
+
+print("Saved video at:", output_file)
